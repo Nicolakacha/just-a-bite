569228ee6de775023c944fff4f3c60e2efa8a7f8
nicolakacha <nicolakacha@gmail.com>
2020-10-18 01:04:17 +0800

    test

--- public/js/question.js
+++ public/js/question.js
@@ -1,10 +1,10 @@
 function init() {
   const questions = document.querySelector(".questions");
   questions.addEventListener("click", (e) => {
-    let target = e.target.closest(".group").getAttribute("data-id");
-    document.querySelectorAll(".answer")[target-1].classList.toggle("hide");
+    if (e.target.classList.contains('question__text')) {
+      e.target.parentNode.parentNode.querySelector('.answer').classList.toggle("hide");
+    }
   });
-  
 }
 
 document.addEventListener("DOMContentLoaded", init);
