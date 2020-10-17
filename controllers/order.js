93ebbccdfd9270a5aa0f04370b429526f4e039f3
nicolakacha <nicolakacha@gmail.com>
2020-10-17 16:42:35 +0800

    test

--- controllers/order.js
+++ controllers/order.js
@@ -6,21 +6,23 @@ const orderController = {
   getCart: (req, res) => {
     let resultArr = [];
     const clientResult = req.body;
-    clientResult.forEach( async(product, index) => {
+    clientResult.forEach(async (product, index) => {
       let item = {};
       let result = await Menu.findOne({
         where: {
           id: product.name,
         },
       });
-      item.id = result.id;
-      item.title = result.title;
-      item.price = result.price;
-      item.quantity = product.number;
-      resultArr.push(item);
-      if (index == clientResult.length - 1) {
-        return res.status(200).json(resultArr);
-      }
+      setTimeout(() => {
+        item.id = result.id;
+        item.title = result.title;
+        item.price = result.price;
+        item.quantity = product.number;
+        resultArr.push(item);
+        if (index == clientResult.length - 1) {
+          return res.status(200).json(resultArr);
+        }
+      }, 300);
     });
   },
 
