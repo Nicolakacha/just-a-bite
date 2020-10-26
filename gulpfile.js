ec6c37b520504bf5cebf57c6326d6a8e9bb84300
nicolakacha <nicolakacha@gmail.com>
2020-10-26 19:20:15 +0800

    compress-picture

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,9 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const rename = require('gulp-rename');
 const cleanCSS = require('gulp-clean-css');
-const imgMin = require('gulp-imagemin');
+const concat = require('gulp-concat');
 
+const imgMin = require('gulp-imagemin');
 
 function compileJS() {
   return src('public/src/js/*.js')
@@ -17,7 +18,7 @@ function compileJS() {
 function compileCSS() {
   return src('public/src/css/*.css')
     .pipe(cleanCSS({ compatibility: 'ie8' }))
-    .pipe(rename({ extname: '.min.css' }))
+    .pipe(concat('main.css'))
     .pipe(dest('public/dist/css'))
 }
 
